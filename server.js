adf6c7bee41e5af2e9de3f2fc9ec053094c7792a
alex <alex.toutongi@gmail.com>
2021-09-08 17:37:59 -0400

    Major Updates to  Website including adding resume.

---
+++ server.js
@@ -0,0 +1,24 @@
+// var livereload = require('livereload');
+// var server = livereload.createServer();
+// server.watch(__dirname + "/docs");
+// var connect  = require('connect');
+// var compiler = require('connect-compiler');
+// var static = require('serve-static');
+//
+// var server = connect();
+//
+// server.use(
+//     compiler({
+//         enabled : [ 'coffee', 'uglify' ],
+//         src     : 'src',
+//         dest    : 'public'
+//     })
+// );
+//
+// server.use(  static(__dirname + '/public'));
+
+// server.listen(3000);
+
+var livereload = require('livereload');
+var lrserver = livereload.createServer();
+lrserver.watch(__dirname + "/docs");
